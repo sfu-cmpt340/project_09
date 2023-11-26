c8e4a8eb3c9e3405f196ab31aa518f27c40dc1a1
Mathew Tse <mathewtse@Mathews-Air.vc.shawcable.net>
2023-11-26 13:58:56 -0800

    Created a navbar, which navigates between two pages Created a new page called 'Processed Images' To do: In the Processed Images, pull all images from the processed images folder on firebase and update the image modification function such that it saves it into a new folder

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,53 +1,18 @@
-import React, { Component } from 'react';
+import { useState, Component, useEffect } from 'react';
+import { storage } from "./firebase";
+import { ref, uploadBytes, listAll } from "firebase/storage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ModifiedPictures from './pages/ModifiedPictures';
+import Home from './pages/Home';
+import NavbarComp from './components/NavbarComp';
+
 import './App.css';
 import axios from 'axios';
 
-class App extends Component {
-
-  state = {
-    selectedFile: null
-  }
- 
-  fileSelectedHandler = event => {
-    this.setState({
-      selectedFile: event.target.files[0]
-    })
-    console.log(event.target.files[0])
-  }
-
-  fileUploadHandler = () => {
-    const fd = new FormData();
-    fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
-    // insert the firebase url below!
-    const post_endpoint = "https://us-central1-project-09-e4dd7.cloudfunctions.net/uploadFile";
-    axios.post(post_endpoint, fd, {
-      onUploadProgress: progressEvent => {
-        console.log('Upload Progress: ' + Math.round((progressEvent.loaded / progressEvent.total) * 100) + '%');
-      }
-    })
-      .then(res => {
-        console.log(res);
-      });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="imagePreview">
-          {/* only render the image if not null */}
-          {this.state.selectedFile != null && <img src={URL.createObjectURL(this.state.selectedFile)} />}
-        </div>
-
-        <div className="returnedImage">
-        </div>
-
-        <div className="uploadButtons">
-          <input type="file" onChange={this.fileSelectedHandler}/>
-          <button onClick={this.fileUploadHandler}>Upload</button>
-        </div>
-      </div>
-    );
-  }
+export default function App(){
+  return (
+    <div>
+      <NavbarComp/>
+    </div>
+  )
 }
-
-export default App;
